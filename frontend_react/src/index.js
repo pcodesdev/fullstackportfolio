e3eb8968bf7a9d7fb4ca9b57be265f4a72044026
Peter Njuguna <njugunapm23@gmail.com>
2022-08-20 11:32:49 +0300

    navbar setup

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./index.css";
+// import reportWebVitals from "./reportWebVitals";
 
-ReactDom.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// reportWebVitals();
