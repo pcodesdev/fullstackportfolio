2b38548c8f43d8a79a7a1617d0d7ceaecee81ea1
Peter Njuguna <njugunapm23@gmail.com>
2022-08-29 05:23:21 +0300

    adding projects to the portfolio

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { motion } from "framer-motion";
+import ReactTooltip from 'react-tooltip';
+
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
+
 
 import "./Skills.scss";
 const Skills = () => {
--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -43,7 +43,7 @@ const Work = () => {
       </h2>
 
       <div className="app__work-filter">
-        {["UI/UX", "Web App", "Mobile App", "React JS", "All"].map(
+        {["UI/UX", "Fullstack", "Web App", "Frontend", "All"].map(
           (item, index) => (
             <div
               key={index}
